91d6a0b9f5b6c56145031d1b061a29201506b8bf
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-04-22 18:54:26 +0530

    Java code to find Size of fundamental data types
    Write a program in C++ to find Size of fundamental data types. 
    Sample Output:
    Find Size of fundamental data types :
    ------------------------------------------
    The sizeof(char) is : 1 bytes
    The sizeof(short) is : 2 bytes
    The sizeof(int) is : 4 bytes
    The sizeof(long) is : 8 bytes
    The sizeof(long long) is : 8 bytes
    The sizeof(float) is : 4 bytes
    The sizeof(double) is : 8 bytes
    The sizeof(long double) is : 16 bytes
    The sizeof(bool) is : 1 bytes

---
+++ SizeofdataTypes.java
@@ -0,0 +1,33 @@
+
+//Program to find Size of fundamental data types
+
+class SizeofdataTypes
+{
+public static void main(String[] args)
+{
+System.out.println("The size of Character is: " + (Character.SIZE/8) + "byte.");
+System.out.println("The size of Short is: " + (Short.SIZE/8) + "bytes.");
+System.out.println("The size of Integer is: " + (Integer.SIZE/8) + "bytes.");
+System.out.println("The size of long is : " + (Long.SIZE/8) + "bytes.");
+//System.out.println("The size of long long is : " + (Long Long.SIZE/8) + "bytes");
+System.out.println("The size of float is : "+ (Float.SIZE/8) + "bytes");
+System.out.println("The size of double is : " + (Double.SIZE/8) + "bytes");
+//System.out.println(" The size of long double is : " + (Long Double.SIZE/8) + "bytes");
+//System.out.println("The size of the Boolean is : " + (Bool.SIZE/8) + "bytes");
+
+}
+
+}
+
+
+
+/*The sizeof(char) is : 1 bytes
+The sizeof(short) is : 2 bytes
+The sizeof(int) is : 4 bytes
+The sizeof(long) is : 8 bytes
+The sizeof(long long) is : 8 bytes
+The sizeof(float) is : 4 bytes
+The sizeof(double) is : 8 bytes
+The sizeof(long double) is : 16 bytes
+The sizeof(bool) is : 1 bytes*/
+
