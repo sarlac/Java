7dcdb2a4b92a5ab93876f5cdca5ba42702c276cd
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-04-23 21:43:53 +0530

    operators, boolean, string, math

---
+++ Program3.java
@@ -0,0 +1,24 @@
+
+//operators, boolean, string, math
+public class Program3
+{
+public static void main(String[] args)
+{
+int x=10,y=20;
+int c=x+y;   //operator
+boolean flag = true;
+int num = 64;
+String firstname="Tom";
+String lastname= "Jerry";
+System.out.println("Value of c : " +c); //string concatination
+System.out.println("Value of flag : " + flag);
+System.out.println(firstname.concat(lastname));  //string concat method
+System.out.println("Square root of num : " + Math.sqrt(num)); //to find square root of string
+System.out.println("Length of first name : " + firstname.length()); // to find length ofthe string
+System.out.println("random number between 0-9 " +Math.random()*10); //to get random number
+System.out.println("absolute value of random : " +Math.abs(-4.56)); // to make a positive number
+}
+}
+
+
+
