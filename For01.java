660272b405c75e5d44e78355d5d4d65af4bdfdc9
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-04-23 22:41:38 +0530

    For loop and while loop
    print the number: 1-10 by using for loop and while loop

---
+++ For01.java
@@ -0,0 +1,22 @@
+//for loop and while loop
+//print the number: 1-10
+
+public class For01
+{
+public static void main(String[] args)
+{
+for(int x=1; x<=10; x++)
+{
+System.out.println("Number : " + x);
+}
+
+System.out.println("\n****While loop Started******");
+
+int num=1;
+while(num<11)
+{
+System.out.println("number : " + num);
+num++;
+}
+}
+}
