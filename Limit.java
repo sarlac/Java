b19534ebbb11462b747cf01bdaeb6a8979312156
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-04-24 16:01:22 +0530

    Upper and lower limit of the integer data type
    Program to find upper and lower limit of the integer data type

---
+++ Limit.java
@@ -0,0 +1,12 @@
+//Program to find upper and lower limit of the integer data type
+
+public class Limit
+{
+public static void main(String[] args)
+{
+
+System.out.println("Min = " + Integer.MIN_VALUE);
+System.out.println("Max = " + Integer.MAX_VALUE);
+
+}
+} 
