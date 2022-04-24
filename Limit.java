6198ef10826614d274d846cc97147f54831d1315
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-04-24 16:27:34 +0530

    Upper and lower limit of data types
    Write a program in java to check the upper and lower limits of integer. 
    
    Expected Output:
    Check the upper and lower limits of integer :
    --------------------------------------------------
    The maximum limit of int data type : 2147483647
    The minimum limit of int data type : -2147483648
    The maximum limit of unsigned int data type : 4294967295
    The maximum limit of long long data type : 9223372036854775807
    The minimum limit of long long data type : -9223372036854775808
    The maximum limit of unsigned long long data type : 18446744073709551615
    The Bits contain in char data type : 8
    The maximum limit of char data type : 127
    The minimum limit of char data type : -128
    The maximum limit of signed char data type : 127
    The minimum limit of signed char data type : -128
    The maximum limit of unsigned char data type : 255
    The minimum limit of short data type : -32768
    The maximum limit of short data type : 32767
    The maximum limit of unsigned short data type : 65535

--- Limit.java
+++ Limit.java
@@ -4,9 +4,27 @@ public class Limit
 {
 public static void main(String[] args)
 {
-
-System.out.println("Min = " + Integer.MIN_VALUE);
-System.out.println("Max = " + Integer.MAX_VALUE);
-
+      System.out.println("The Minimum limit of int data type : " + Integer.MIN_VALUE);
+      System.out.println("The maximum limit of int data type : " + Integer.MAX_VALUE);
+	  
+	  //System.out.println("The Minimum limit of Unsigned data type : " + Unsigned Integer.MIN_VALUE);
+      //System.out.println("The maximum limit of Unsigned data type : " + Unsigned Integer.MAX_VALUE);
+	  
+	  System.out.println("The Minimum limit of long long data type : " + Long.MIN_VALUE);
+      System.out.println("The maximum limit of long long data type : " + Long.MAX_VALUE);
+	  
+	  System.out.println("The Minimum limit of Char data type : " + Byte.MIN_VALUE);
+      System.out.println("The maximum limit of Char data type : " + Byte.MAX_VALUE);
+	  
+	  System.out.println("The Minimum limit of Short data type : " + Short.MIN_VALUE);
+      System.out.println("The maximum limit of Short data type : " + Short.MAX_VALUE);
+	  
+	  System.out.println("The Minimum limit of Float data type : " + Float.MIN_VALUE);
+      System.out.println("The maximum limit of Float data type : " + Float.MAX_VALUE);
+	  
+	  System.out.println("The Minimum limit of double data type : " + Double.MIN_VALUE);
+      System.out.println("The maximum limit of double data type : " + Double.MAX_VALUE);
+	  
+   
 }
 } 
