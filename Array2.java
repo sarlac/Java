36d6ded4a86d3c26dc934e17f5a1a6add2d08010
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-04-25 21:56:45 +0530

    program to implement array in java

---
+++ Array2.java
@@ -0,0 +1,14 @@
+//program to implement arrays
+
+public class Array2
+{
+public static void main(String[] args)
+{
+int i;
+String[] cars={"volvo","bmw","ford","mazda"};
+for(i=0;i<=3;i++)
+{
+System.out.println(cars[i]);
+}
+}
+}
