ea68a552e7d55d99767e584d666fcb2cad50688a
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-04-25 21:35:27 +0530

    program to widening typecast
    Widening casting is done automatically when passing a smaller size type to a larger size type:

---
+++ Typecast.java
@@ -0,0 +1,14 @@
+//Program for type casting
+
+public class Typecast
+{
+public static void main(String[] args)
+{
+int myInt=9;
+double mydouble=myInt;
+
+System.out.println(myInt);
+System.out.println(mydouble);
+
+}
+}
