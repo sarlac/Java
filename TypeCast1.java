126cfbcadd7b7eb5f1c773dffc88b2b94cdb2937
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-04-25 21:43:24 +0530

    program for narrowing typecast
    Narrowing casting must be done manually by placing the type in parentheses in front of the value:

---
+++ TypeCast1.java
@@ -0,0 +1,14 @@
+//program to type cast: narrowing casting
+
+public class TypeCast1
+{
+public static void main(String[] args)
+{
+double myDouble=9.78;
+int myInt=(int)myDouble;
+
+System.out.println(myDouble);
+System.out.println(myInt);
+
+}
+}
